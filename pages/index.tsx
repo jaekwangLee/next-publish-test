6c1dfde41fe691d08b5b2fa0e1b470b5ea047f80
jklee <jklee@jkleeui-MacBookAir.local>
2022-10-29 22:00:05 +0900

    BUILD: 빌드 파일 포함해서 커밋

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Re-Updated Welcome to <a 
+          ReRe-Updated Welcome to <a 
 href="https://nextjs.org">Next.js!</a>
         </h1>
 
