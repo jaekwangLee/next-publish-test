e73e890beeadb66afd1c9e0ae8d6f5684b145d0f
jklee <jklee@jkleeui-MacBookAir.local>
2022-11-03 07:26:25 +0900

    FEAT: page 주석 추가

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,5 +1,11 @@
 import { AddressProvider } from '@components/address/AddressProvider';
 
+/******
+ * Layout안에 여러 Provier를 넣는다거나
+ * Container로 Provier를 감싼다거나
+ * 그냥 Provider하나만 위치시키거나 경우에따라 처리
+ ****/
+
 function MyPage() {
   return (
     <>
